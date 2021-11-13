1fce0eb082ce359cb4eedb32ab5ab1884efc9faa
Ivan Ramos <iardoru@gmail.com>
2021-11-12 21:49:04 -0500

    Add more files

---
+++ docker-compose-start.ps1
@@ -0,0 +1,8 @@
+docker-compose -f ./adminer/docker-compose.yml up -d
+docker-compose -f ./apache/docker-compose.yml up -d
+docker-compose -f ./aria2-pro/docker-compose.yml up -d
+docker-compose -f ./nginx/docker-compose.yml up -d
+docker-compose -f ./portainer/docker-compose.yml up -d
+docker-compose -f ./traefik/docker-compose.yml up -d
+docker-compose -f ./whoami/docker-compose.yml up -d
+
