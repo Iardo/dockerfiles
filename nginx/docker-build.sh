d1fdbc2142cc5f7b2ff172968cd454d9d2e2989f
Ivan Ramos <iardoru@gmail.com>
2022-06-04 03:33:18 -0500

    Updates

--- nginx/docker-build.sh
+++ nginx/docker-build.sh
@@ -2,9 +2,9 @@
 # set -o allexport; source .env; set +o allexport
 
 # check if the folders exist before volume binding
-if ! [[ -d ./www ]]; then
-  mkdir -p ./www
-  echo directory created \"www\"
+if ! [[ -d ./web ]]; then
+    mkdir -p ./web
+    echo directory created \"web\"
 fi
 
 # force to use BUILDKIT instead of the old Docker build system
@@ -15,17 +15,15 @@ docker-compose build --progress=plain --no-cache
 docker-compose -f docker-compose.yml up -d
 
 # copy default index.html
-if ! [[ -d ./www/html ]]; then
-  mkdir -p ./www/html
-  tar -xvzf ./default-html.tar.gz -C ./www/html
+if ! [[ -d ./web/html ]]; then
+    mkdir -p ./web/html
+    tar -xvzf ./default-html.tar.gz -C ./web/html
 fi
 
 # create static phpinfo.php
-if ! [[ -d ./www/html/phpinfo ]]; then
-    mkdir -p ./www/html/phpinfo
-  {
-    echo "<?php phpinfo();"
-  } >> ./www/html/phpinfo/index.php
+if ! [[ -d ./web/html/phpinfo ]]; then
+    mkdir -p ./web/html/phpinfo
+    {
+        echo "<?php phpinfo();"
+    } >>./www/html/phpinfo/index.php
 fi
-
-
