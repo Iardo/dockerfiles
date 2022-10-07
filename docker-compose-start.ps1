d6cbeed5f8546c0314267469ba4b3d14c0132ab4
Ivan Ramos <iramos@rootstack.com>
2022-10-07 18:22:54 -0500

    delete unnecesary shellscripts

--- docker-compose-start.ps1
+++
@@ -1,7 +0,0 @@
-docker-compose -f ./traefik/docker-compose.yml up -d
-docker-compose -f ./adminer/docker-compose.yml up -d
-docker-compose -f ./apache/docker-compose.yml up -d
-docker-compose -f ./aria2-pro/docker-compose.yml up -d
-docker-compose -f ./nginx/docker-compose.yml up -d
-docker-compose -f ./portainer/docker-compose.yml up -d
-docker-compose -f ./whoami/docker-compose.yml up -d
