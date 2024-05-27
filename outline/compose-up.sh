400815c278bcf52154d0af4f75d191b356af5843
Ivan Ramos <iardoru@gmail.com>
2024-05-27 03:56:44 -0500

    update outline

--- outline/compose-up.sh
+++ outline/compose-up.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cp .env.sample .env
+if ! [ -f .env ]; then
+  cp .env.sample .env
+fi
+
 docker-compose up -d
 docker compose run --rm outline yarn db:create --env=production-ssl-disabled
 docker compose run --rm outline yarn db:migrate --env=production-ssl-disabled
