0d688e9ed4e0b8d3c81dc77f69f7fb3415aeab2f
Ivan Ramos <iardoru@gmail.com>
2024-06-02 21:50:47 -0500

    update open-project

--- open-project/deploy/caddyfile.proxy.sh
+++ open-project/deploy/caddyfile.proxy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-sed 's|${APP_HOST}|'"$APP_HOST"'|g' /etc/caddy/Caddyfile.template > /etc/caddy/Caddyfile
+sed 's|${APP_HOST}|'"$APP_HOST"'|g' /etc/caddy/caddyfile.template > /etc/caddy/Caddyfile
 caddy run --config /etc/caddy/Caddyfile
