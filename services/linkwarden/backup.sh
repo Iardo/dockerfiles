3a2ea41a22b2c99565bb068250ccb52e3130715e
Ivan Ramos <iardoru@gmail.com>
2024-06-08 18:02:00 -0500

    update linkwarden

---
+++ services/linkwarden/backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# Docker container names
+CONTAINER_LINKWARDEN="linkwarden-web"
+CONTAINER_DATABASE="linkwarden-database"
+FOLDER_BACKUPS="./backups"
+TIMESTAMP=$(date +"%Y.%m.%d-%H.%M.%S")
+
+# Create Backups folder
+mkdir -p $FOLDER_BACKUPS
+
+# Create Temporary folder
+mkdir -p $TIMESTAMP-backup
+
+# Dump DB into SQL File
+echo -n "Exporting database ..."
+docker exec -t $CONTAINER_DATABASE pg_dumpall -c -U postgres > $TIMESTAMP-backup/postgres.sql
+echo "Success!"
+
+# Create tgz
+echo -n "Generating tarball $TIMESTAMP-backup.tgz ..."
+tar -czf $FOLDER_BACKUPS/$TIMESTAMP-backup.tgz \
+  $TIMESTAMP-backup/postgres.sql \
+echo "Success!"
+
+# Remove source files
+echo -n "Cleaning up temporary files ..."
+rm -rf $TIMESTAMP-backup
+echo "Success!"
+
+echo -e "\nBackup Complete!\n"
---
+++ services/linkwarden/restore.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# Docker container names
+CONTAINER_LINKWARDEN="linkwarden-web"
+CONTAINER_POSTGRES="linkwarden-database"
+BACKUP_ARCHIVE_TGZ=$1
+BACKUP_ARCHIVE=$(basename $BACKUP_ARCHIVE_TGZ .tgz)
+
+LOG_FOLDER="./logs"
+LOG_FILE=$LOG_FOLDER/${BACKUP_ARCHIVE}-restore.txt
+
+# Create Logs folder
+mkdir -p $LOG_FOLDER
+
+# Extract tgz archive
+echo -n "Extracting tarball $BACKUP_ARCHIVE_TGZ ..."
+tar -xzf $BACKUP_ARCHIVE_TGZ
+echo "Success!"
+
+# Import Database
+echo -n "Importing database ..."
+cat $BACKUP_ARCHIVE/postgres.sql | docker exec -i $CONTAINER_POSTGRES psql -U postgres -q > $LOG_FILE
+echo "Success!"
+
+echo -n "Cleaning up temporary files and folders ..."
+rm -r $BACKUP_ARCHIVE
+echo "Success!"
+
+echo -e "\nRestore complete!\n"
