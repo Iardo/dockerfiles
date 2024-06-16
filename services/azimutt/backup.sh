58774553c6dab8f3d1c7a7f36baf95004e62f2c5
Ivan Ramos <iardoru@gmail.com>
2024-06-16 12:46:21 -0500

    update azimutt

--- services/azimutt/backup.sh
+++ services/azimutt/backup.sh
@@ -19,24 +19,10 @@ echo -n "Exporting database ..."
 docker exec -t $CONTAINER_DATABASE pg_dumpall -c -U postgres > $TIMESTAMP-backup/postgres.sql
 echo "Success!"
 
-# Export Docker Volumes
-echo -n "Exporting images-avatars ..."
-docker run --rm --volumes-from $CONTAINER_WEB -v $(pwd)/$TIMESTAMP-backup:/backup ubuntu cp -r /app/public/user-avatars /backup/user-avatars
-echo "Success!"
-echo -n "Exporting images-background ..."
-docker run --rm --volumes-from $CONTAINER_WEB -v $(pwd)/$TIMESTAMP-backup:/backup ubuntu cp -r /app/public/project-background-images /backup/project-background-images
-echo "Success!"
-echo -n "Exporting attachments ..."
-docker run --rm --volumes-from $CONTAINER_WEB -v $(pwd)/$TIMESTAMP-backup:/backup ubuntu cp -r /app/private/attachments /backup/attachments
-echo "Success!"
-
 # Create tgz
 echo -n "Generating tarball $TIMESTAMP-backup.tgz ..."
 tar -czf $FOLDER_BACKUPS/$TIMESTAMP-backup.tgz \
-  $TIMESTAMP-backup/postgres.sql \
-  $TIMESTAMP-backup/user-avatars \
-  $TIMESTAMP-backup/project-background-images \
-  $TIMESTAMP-backup/attachments
+  $TIMESTAMP-backup/postgres.sql
 echo "Success!"
 
 # Remove source files
