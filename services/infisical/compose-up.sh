8a12c96de3dff98c6ed012e5b3537b3b44b13c87
Ivan Ramos <iardoru@gmail.com>
2024-06-16 15:47:10 -0500

    update infisical

--- services/infisical/compose-up.sh
+++ services/infisical/compose-up.sh
@@ -5,7 +5,7 @@ set -o pipefail
 if ! [ -f .env ]; then
   cp .env.example .env
 
-  secret=$(openssl rand -hex 32)
+  secret=$(openssl enc -aes-128-cbc -iter 10000 -nosalt -P -k secret -md sha1 | awk -F= '$1 == "key" {print $2}')
   sed -i "s/=secret/=$secret/" .env
 fi
 
